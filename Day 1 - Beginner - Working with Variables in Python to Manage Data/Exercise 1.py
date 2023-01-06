11eb8e885748b79abfa8ca6ded010898fca8c558
therealyash <yashraj1416@gmail.com>
2023-01-06 11:29:13 +0530

    Updated Solutions!

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/Exercise 1.py
@@ -0,0 +1,7 @@
+'''Write a program in main.py that prints the
+notes from the previous lesson using what you
+have learnt about the Python print function.'''
+
+print("100 Days of Python")
+print("This is the Day 1.")
+print("We can print single quotes like this ' '.")
